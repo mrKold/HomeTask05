ec009f3bac3496ec5eac72e5927098e4b2d1518e
IvanK <mr.koldamasov@mail.ru>
2022-10-14 16:07:40 +0300

    определяет, присутствует ли заданное число в массиве

---
+++ Ex033/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив. Напишите программу, которая определяет, 
+//присутствует ли заданное число в массиве.
+//4; массив [6, 7, 19, 345, 3] -> нет
+//3; массив [6, 7, 19, 345, 3] -> да
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10);
+}
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string Find(int[] array)
+{
+    string result = "нет";
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ( array[i] == n)
+            result = "да";
+    }
+    return result;
+}
+int[] array = new int[4];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine($"Результат: {Find(array)}");
