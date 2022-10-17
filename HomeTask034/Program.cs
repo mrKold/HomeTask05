c79e6c642e71a4ff2fa57541b5f8a6d9673bceea
IvanK <mr.koldamasov@mail.ru>
2022-10-17 19:23:56 +0300

    выводит количество чётных чисел в массиве

---
+++ HomeTask034/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+
+
+void InputArray(int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(100, 1000);
+}
+
+int Numbers(int[] array)
+{
+    int P = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            P = P + 1;
+    }
+    return P;
+}
+Console.Clear();
+int[] array = new int[5];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Numbers(array);
+Console.WriteLine($"Результат: {Numbers(array)}");
